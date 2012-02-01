9e0bf4d5e57c02ab15af5572d283271e9e07a460
driver station <buchananrobotics@gmail.com>
2012-01-31 17:02:47 -0800

    On branch master  Changes to be committed: 	new file:   Practice/CustomDrive.cpp 	new file:   Practice/CustomDrive.h 	new file:   Practice/Doc7.cpp 	new file:   Practice/Doc7.h

---
+++ Practice/Doc7.h
@@ -0,0 +1,40 @@
+#ifndef _DOC7_H_
+#define _DOC7_H_
+
+#include "CustomDrive.h" 
+#include "Arm.h"
+#include "Manipulator.h"
+#include "Tracking.h"
+
+#define NUM_JAGS 4
+#define DOC7_GYRO_PORT 1
+
+#define DOC7_GAMEPAD 1
+#define DOC7_ARMPAD 2
+
+#define DOC7_ARM_JAG 5
+#define DOC7_MAN_JAG 6
+
+#define DOC7_RELAY_PORT 1
+#define DOC7_COMPRESSOR_PORT 1
+
+#define DOC7_SOL_OPEN 2
+#define DOC7_SOL_CLOSE 1
+
+#define DOC7_ARM_A_CHANNEL 2
+#define DOC7_ARM_B_CHANNEL 3
+#define DOC7_MAN_A_CHANNEL 4
+#define DOC7_MAN_B_CHANNEL 5
+
+#define DOC7_LIMIT_SWITCH_PORT 14
+
+#define DOC7_LEFT_LINE_PORT 6
+#define DOC7_CENTER_LINE_PORT 7
+#define DOC7_RIGHT_LINE_PORT 8
+
+#define DOC7_RED_WHITE_SPIKE 2
+#define DOC7_BLUE_SPIKE 3
+
+
+
+#endif
